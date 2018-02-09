cb691f69b3a634540feb368f293e04ec9f54fb56
Brandon Watkins <brandonwatkins1@mhu.edu>
2018-02-09 11:51:10 -0500

    Added logo_with_name and splash_screen_background Need to rename the files in the hidden drawable folders Created a SplashActivity

---
+++ app/src/main/java/com/example/android/studyspotapp/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.studyspotapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brandonwatkins on 9/02/18.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
