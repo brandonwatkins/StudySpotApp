d2a30e07f3aca5e811003b779d42cbb0f81e0360
Brandon Watkins <brandonwatkins1@mhu.edu>
2018-02-09 21:12:27 -0500

    Added MyRecordedHoursActivity with TabAllSessions and TabThisWeek. Also added code for My Hours and View StudySpot buttons.

---
+++ app/src/main/java/com/example/android/studyspotapp/TabThisWeek.java
@@ -0,0 +1,25 @@
+package com.example.android.studyspotapp;
+
+//import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by brandonwatkins on 9/02/18.
+ */
+
+public class TabThisWeek extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_this_week, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("This week");
+        return rootView;
+    }
+}
