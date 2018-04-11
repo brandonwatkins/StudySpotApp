081a5bda236475b1d1b2f1fe79b42b6f1852846b
Brandon Watkins <brandonwatkins1@mhu.edu>
2018-04-10 22:10:25 -0400

    Formatted the sessionlength

--- app/src/main/java/com/example/android/studyspotapp/Database/StudySession.java
+++ app/src/main/java/com/example/android/studyspotapp/Database/StudySession.java
@@ -3,6 +3,11 @@ package com.example.android.studyspotapp.Database;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by brandonwatkins on 13/02/18.
  */
@@ -58,12 +63,29 @@ public class StudySession {
     }
 
     public String getStudySessionDisplay(int sessionLength) {
-        int length = sessionLength;
+        /*int length = sessionLength;
         length /= 1000;
         int min = length / 60;
         length %= 60;
 
-        return Integer.toString(length);
+        return Integer.toString(length);*/
+
+        /*int seconds = sessionLength / 1000;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        int hours = minutes / 60;
+        minutes = minutes % 60;
+
+        return (hours + ":" + minutes + ":" + seconds);*/
+
+        Date time = new Date(sessionLength);
+        DateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String timeFormattedDate = format.format(time);
+
+        return timeFormattedDate;
+
+
+
     }
 
     public void setSessionLength(int sessionLength) {
